911062a31c3dea840e634edd2e4245540c60ff7d
SAcollective <97stevenanderson@gmail.com>
2024-09-23 15:35:39 -0400

    Update Blink_Led.ino
    MY_LED has been changed to ledPin

--- Blink_Led.ino
+++ Blink_Led.ino
@@ -1,10 +1,10 @@
 // We define MY_LED to be the LED that we want to blink.
-//
+// Updated MY_LED to ledPin
 
 #include "Particle.h"
 // In this tutorial, we're using the blue D7 LED (next to D7 on the Photon
 // and Electron, and next to the USB connector on the Argon and Boron).
-const pin_t MY_LED = D7;
+const pin_t ledPin = D7;
 
 // The following line is optional, but recommended in most firmware. It
 // allows your code to run before the cloud is connected. In this case,
@@ -23,23 +23,36 @@ void setup()
 	// In order to set a pin, you must tell Device OS that the pin is
 	// an OUTPUT pin. This is often done from setup() since you only need
 	// to do it once.
-	pinMode(MY_LED, OUTPUT);
+	pinMode(ledPin, OUTPUT);
 }
 
 // The loop() method is called frequently.
-void loop()
-{
-	// Turn on the LED
-	digitalWrite(MY_LED, HIGH);
-
-	// Leave it on for one second
-	delay(3s);
-
-	// Turn it off
-	digitalWrite(MY_LED, LOW);
-
-	// Wait one more second
-	delay(1s);
-
-	// And repeat!
+void loop() {
+  // Three short flashes to represent the letter "S"
+  for (int x = 0; x < 3; x++) {
+    digitalWrite(ledPin, HIGH);   // Set LED to ON
+    delay(150);                   // Delay for 150 milliseconds
+    digitalWrite(ledPin, LOW);    // Set LED to OFF
+    delay(100);                   // Delay for 100 milliseconds
+  }
+  // Delay of 200 milliseconds to create a gap between letters
+  delay(200);
+  // Three short flashes to represent the letter "O"
+  for (int x = 0; x < 3; x++) {
+    digitalWrite(ledPin, HIGH);   // Set LED to ON
+    delay(400);                   // Delay for 400 milliseconds
+    digitalWrite(ledPin, LOW);    // Set LED to OFF
+    delay(200);                   // Delay for 200 milliseconds
+  }
+  // Delay of 100 milliseconds to create a gap between letters
+  delay(100);
+  // Three short flashes to represent the letter "S" again
+  for (int x = 0; x < 3; x++) {
+    digitalWrite(ledPin, HIGH);   // Set LED to ON
+    delay(150);                   // Delay for 150 milliseconds
+    digitalWrite(ledPin, LOW);    // Set LED to OFF
+    delay(100);                   // Delay for 100 milliseconds
+  }
+  // Wait for 5 seconds before repeating the S.O.S signal
+  delay(5000);
 }
